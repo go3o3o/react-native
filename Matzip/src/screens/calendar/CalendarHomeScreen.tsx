0b2274430d734c87cea18cd5e4cd5828b945749d
hoyoni <hoyeon.kim2773@gmail.com>
2024-08-16 08:43:53 +0900

    Matzip 앱 > home screen

---
+++ Matzip/src/screens/calendar/CalendarHomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function CalendarHomeScreen() {
+  return (
+    <View>
+      <Text>캘린더</Text>
+    </View>
+  );
+}
