acc9ce9de23bcfd2d21a488e990565d5e1c75307
hoyoni <hoyeon.kim2773@gmail.com>
2024-08-22 11:14:53 +0900

    Matzip 앱 > color 추가

--- Matzip/src/constants/colors.ts
+++ Matzip/src/constants/colors.ts
@@ -1,5 +1,11 @@
 export const colors = {
-  PINK_700: '#c63b64',
-  PINK_500: '#bf5c79',
-  WHITE: '#ffffff',
+  PINK_500: '#BF5C79',
+  PINK_700: '#C63B64',
+  RED_300: '#FFB4B4',
+  RED_500: '#FF5F5F',
+  WHITE: '#FFF',
+  GRAY_200: '#E7E7E7',
+  GRAY_500: '#8E8E8E',
+  GRAY_700: '#575757',
+  BLACK: '#000',
 };
