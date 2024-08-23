b2424d282bbdfc40a9b95b98a3a856152239e4b7
hoyoni <hoyeon.kim2773@gmail.com>
2024-08-23 14:13:01 +0900

    Matzip 앱 > 로그인 화면 수정

--- Matzip/src/screens/auth/LoginScreen.tsx
+++ Matzip/src/screens/auth/LoginScreen.tsx
@@ -1,10 +1,62 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useRef} from 'react';
+import {SafeAreaView, StyleSheet, TextInput, View} from 'react-native';
+import InputField from '../../components/InputField';
+import CustomButton from '../../components/CustomButton';
+import {validateLogin} from '../../utils/validate';
+import useForm from '../../hooks/useForm';
 
 export default function LoginScreen() {
+  const passwordRef = useRef<TextInput | null>(null);
+  const login = useForm({
+    initialValue: {email: '', password: ''},
+    validate: validateLogin,
+  });
+  const handleSubmit = () => {
+    console.log('login.values', login.values);
+  };
+
   return (
-    <View>
-      <Text>로그인</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          autoFocus
+          placeholder="이메일"
+          error={login.errors.email}
+          touched={login.touched.email}
+          inputMode="email"
+          returnKeyType="next"
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordRef.current?.focus()}
+          {...login.getTextInputProps('email')}
+        />
+        <InputField
+          ref={passwordRef}
+          placeholder="비밀번호"
+          error={login.errors.password}
+          touched={login.touched.password}
+          secureTextEntry
+          returnKeyType="join"
+          onSubmitEditing={handleSubmit}
+          {...login.getTextInputProps('password')}
+        />
+      </View>
+      <CustomButton
+        label="로그인"
+        variant="filled"
+        size="large"
+        onPress={handleSubmit}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
