abfc57e10671dd50b0be99c6790efa0ae19a6a16
hoyoni <hoyeon.kim2773@gmail.com>
2024-09-06 11:22:00 +0900

    Matzip 앱 > constant navigation main, map 추가

--- Matzip/src/constants/navigations.ts
+++ Matzip/src/constants/navigations.ts
@@ -1,5 +1,15 @@
+export const mainNavigations = {
+  HOME: 'Home',
+  FEED: 'Feed',
+  CALENDAR: 'Calendar',
+};
+
 export const authNavigations = {
   AUTH_HOME: 'AuthHome',
   LOGIN: 'Login',
   SIGNUP: 'Signup',
+};
+
+export const mapNavigations = {
+  MAP_HOME: 'MapHome',
 };
