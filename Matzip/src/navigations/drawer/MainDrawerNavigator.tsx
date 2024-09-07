385647e09c9668c63320ecc09fdc99b0d3c6248d
hoyoni <hoyeon.kim2773@gmail.com>
2024-09-07 13:07:26 +0900

    Matzip 앱 > main drawer navigator

--- Matzip/src/navigations/drawer/MainDrawerNavigator.tsx
+++ Matzip/src/navigations/drawer/MainDrawerNavigator.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import MapHomeScreen from '../../screens/map/MapHomeScreen';
-import FeedHomeScreen from '../../screens/feed/FeedHomeScreen';
-import CalendarHomeScreen from '../../screens/calendar/CalendarHomeScreen';
+import FeedHomeScreen from '@/screens/feed/FeedHomeScreen';
+import CalendarHomeScreen from '@/screens/calendar/CalendarHomeScreen';
+import {mainNavigations} from '@/constants';
+import {MapStackNavigator} from '../stack/MapStackNavigator';
 
-const Drawer = createDrawerNavigator();
+export type MainDrawerParamList<navigationName extends string> = {
+  [k in navigationName]: undefined;
+};
+
+const Drawer = createDrawerNavigator<MainDrawerParamList<string>>();
 
 export default function MainDrawerNavigator() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="MapHome" component={MapHomeScreen} />
-      <Drawer.Screen name="FeedHome" component={FeedHomeScreen} />
-      <Drawer.Screen name="CalendarHome" component={CalendarHomeScreen} />
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: false,
+        drawerType: 'front',
+      }}>
+      <Drawer.Screen
+        name={mainNavigations.HOME}
+        component={MapStackNavigator}
+        options={{
+          title: '홈',
+        }}
+      />
+      <Drawer.Screen
+        name={mainNavigations.FEED}
+        component={FeedHomeScreen}
+        options={{
+          title: '피드',
+        }}
+      />
+      <Drawer.Screen
+        name={mainNavigations.CALENDAR}
+        component={CalendarHomeScreen}
+        options={{
+          title: '캘린더',
+        }}
+      />
     </Drawer.Navigator>
   );
 }
