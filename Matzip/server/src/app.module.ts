f49655fcf80dee2cfba190b1aebc49564bfa45b7
hoyoni <hoyeon.kim2773@gmail.com>
2024-10-04 09:21:00 +0900

    Matzip 서버 > app module

--- Matzip/server/src/app.module.ts
+++ Matzip/server/src/app.module.ts
@@ -1,10 +1,41 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+
+import { AuthModule } from './auth/auth.module';
+import { ImageModule } from './image/image.module';
+import { PostModule } from './post/post.module';
+import { FavoriteModule } from './favorite/favorite.module';
+import { LoggerMiddleware } from './@common/middlewares/logger.middleware';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/',
+    }),
+    PostModule,
+    AuthModule,
+    ImageModule,
+    FavoriteModule,
+  ],
+  providers: [ConfigService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
