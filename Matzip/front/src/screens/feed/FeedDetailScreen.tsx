c17914cd1b5b67080c3d9c8b633281c68e19afbe
hoyoni <hoyeon.kim2773@gmail.com>
2024-11-08 10:40:46 +0900

    Matzip 앱 > feed detail screen

---
+++ Matzip/front/src/screens/feed/FeedDetailScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+interface FeedDetailScreenProps {}
+function FeedDetailScreen({}: FeedDetailScreenProps) {
+  return <View></View>;
+}
+const styles = StyleSheet.create({});
+export default FeedDetailScreen;
