028595a85b89d50a5143e4374f19538ea6a0c94f
hoyoni <hoyeon.kim2773@gmail.com>
2024-11-16 10:58:28 +0900

    Matzip 앱 > hook use move map view

---
+++ Matzip/front/src/hooks/useMoveMapView.ts
@@ -0,0 +1,33 @@
+import {numbers} from '@/constants';
+import useLocationStore from '@/store/useLocationStore';
+import {useEffect, useRef, useState} from 'react';
+import MapView, {LatLng, Region} from 'react-native-maps';
+
+type Delta = Pick<Region, 'latitudeDelta' | 'longitudeDelta'>;
+
+function useMoveMapView() {
+  const mapRef = useRef<MapView | null>(null);
+  const [regionDelta, setRegionDelta] = useState<Delta>(numbers.INITIAL_DELTA);
+  const {moveLocation} = useLocationStore();
+
+  const moveMapView = (coordinate: LatLng, delta?: Delta) => {
+    mapRef.current?.animateToRegion({
+      ...coordinate,
+      ...(delta ?? regionDelta),
+    });
+  };
+
+  const handleChangeDelta = (region: Region) => {
+    const {latitudeDelta, longitudeDelta} = region;
+    setRegionDelta({latitudeDelta, longitudeDelta});
+  };
+
+  useEffect(() => {
+    moveLocation && moveMapView(moveLocation);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [moveLocation]);
+
+  return {mapRef, moveMapView, handleChangeDelta};
+}
+
+export default useMoveMapView;
