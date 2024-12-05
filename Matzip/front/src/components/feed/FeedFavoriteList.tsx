a90dfb6a90aed73a461f0d69d3352a1134ab9deb
hoyoni <hoyeon.kim2773@gmail.com>
2024-12-05 11:54:07 +0900

    Matzip 앱 > component feed favorite list

--- Matzip/front/src/components/feed/FeedFavoriteList.tsx
+++ Matzip/front/src/components/feed/FeedFavoriteList.tsx
@@ -1,21 +1,56 @@
-import React from 'react';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import FeedItem from './FeedItem';
+import useGetInfiniteFavoritePosts from '@/hooks/queries/useGetInfiniteFavoritePosts';
 
-import {colors} from '@/constants';
-import FeedFavoriteList from '@/components/feed/FeedFavoriteList';
+function FeedFavoriteList() {
+  const {
+    data: posts,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    refetch,
+  } = useGetInfiniteFavoritePosts();
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
-function FeedFavoriteScreen() {
+  const handleRefresh = async () => {
+    setIsRefreshing(true);
+    await refetch();
+    setIsRefreshing(false);
+  };
+
+  const handleEndReached = () => {
+    if (hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <FeedFavoriteList />
-    </SafeAreaView>
+    <FlatList
+      data={posts?.pages.flat()}
+      renderItem={({item}) => <FeedItem post={item} />}
+      keyExtractor={item => String(item.id)}
+      numColumns={2}
+      contentContainerStyle={styles.contentContainer}
+      ListEmptyComponent={
+        <View>
+          <Text style={{textAlign: 'center'}}>즐겨찾기한 장소가 없습니다.</Text>
+        </View>
+      }
+      onEndReached={handleEndReached}
+      onEndReachedThreshold={0.5}
+      refreshing={isRefreshing}
+      onRefresh={handleRefresh}
+      scrollIndicatorInsets={{right: 1}}
+      indicatorStyle="black"
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.WHITE,
+  contentContainer: {
+    padding: 15,
   },
 });
-export default FeedFavoriteScreen;
+
+export default FeedFavoriteList;
