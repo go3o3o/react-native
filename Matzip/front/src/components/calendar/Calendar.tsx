4b65b3d433521bfd9bb2662520ba22dec10ecaa2
hoyoni <hoyeon.kim2773@gmail.com>
2024-12-10 08:45:33 +0900

    Matzip 앱 > component calendar

---
+++ Matzip/front/src/components/calendar/Calendar.tsx
@@ -0,0 +1,102 @@
+import {colors} from '@/constants';
+import React from 'react';
+import {FlatList, Text} from 'react-native';
+import {Pressable, StyleSheet, View} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import DayOfWeeks from './DayOfWeeks';
+import {MonthYear, isSameAsCurrentDate} from '@/utils';
+import DateBox from './DateBox';
+
+interface CalendarProps {
+  monthYear: MonthYear;
+  selectedDate: number;
+  onPressDate: (date: number) => void;
+  onChangeMonth: (increment: number) => void;
+}
+
+function Calendar({
+  monthYear,
+  selectedDate,
+  onPressDate,
+  onChangeMonth,
+}: CalendarProps) {
+  const {month, year, lastDate, firstDOW} = monthYear;
+
+  return (
+    <>
+      <View style={styles.headerContainer}>
+        <Pressable
+          onPress={() => onChangeMonth(-1)}
+          style={styles.monthButtonContainer}>
+          <Ionicons name="arrow-back" size={25} color={colors.BLACK} />
+        </Pressable>
+        <Pressable style={styles.monthYearContainer}>
+          <Text style={styles.titleText}>
+            {year}년 {month}월
+          </Text>
+          <MaterialIcons
+            name="keyboard-arrow-down"
+            size={20}
+            color={colors.GRAY_500}
+          />
+        </Pressable>
+        <Pressable
+          onPress={() => onChangeMonth(1)}
+          style={styles.monthButtonContainer}>
+          <Ionicons name="arrow-forward" size={25} color={colors.BLACK} />
+        </Pressable>
+      </View>
+
+      <DayOfWeeks />
+      <View style={styles.bodyContainer}>
+        <FlatList
+          data={Array.from({length: lastDate + firstDOW}, (_, i) => ({
+            id: i,
+            date: i - firstDOW + 1,
+          }))}
+          renderItem={({item}) => (
+            <DateBox
+              date={item.date}
+              isToday={isSameAsCurrentDate(year, month, item.date)}
+              selectedDate={selectedDate}
+              onPressDate={onPressDate}
+            />
+          )}
+          keyExtractor={item => String(item.id)}
+          numColumns={7}
+        />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 25,
+    marginVertical: 16,
+  },
+  monthYearContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  monthButtonContainer: {
+    padding: 10,
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: colors.BLACK,
+  },
+  bodyContainer: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: colors.GRAY_300,
+    backgroundColor: colors.GRAY_100,
+  },
+});
+
+export default Calendar;
