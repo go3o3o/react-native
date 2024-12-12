a5bc769173cf492693a3c39e1c821dba3c23b600
hoyoni <hoyeon.kim2773@gmail.com>
2024-12-12 13:04:52 +0900

    Matzip 앱 > calendar home screen

--- Matzip/front/src/screens/calendar/CalendarHomeScreen.tsx
+++ Matzip/front/src/screens/calendar/CalendarHomeScreen.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import Calendar from '@/components/calendar/Calendar';
+import {colors} from '@/constants';
+import {getMonthYearDetails, getNewMonthYear} from '@/utils';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
-export default function CalendarHomeScreen() {
+function CalendarHomeScreen() {
+  const currentMonthYear = getMonthYearDetails(new Date());
+  const [monthYear, setMonthYear] = useState(currentMonthYear);
+  const [selectedDate, setSelectedDate] = useState(0);
+
+  const handlePressDate = (date: number) => {
+    setSelectedDate(date);
+  };
+
+  const handleUpdateMonth = (increment: number) => {
+    setMonthYear(prev => getNewMonthYear(prev, increment));
+  };
+
   return (
-    <View>
-      <Text>캘린더</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Calendar
+        monthYear={monthYear}
+        onChangeMonth={handleUpdateMonth}
+        selectedDate={selectedDate}
+        onPressDate={handlePressDate}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.WHITE,
+  },
+});
+
+export default CalendarHomeScreen;
