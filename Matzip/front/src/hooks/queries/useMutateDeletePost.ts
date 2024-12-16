ef10acf0780e65dbb4ce5788879cdf1c6ae601c0
hoyoni <hoyeon.kim2773@gmail.com>
2024-12-16 10:27:26 +0900

    Matzip 앱 > add missing invalidate queries

--- Matzip/front/src/hooks/queries/useMutateDeletePost.ts
+++ Matzip/front/src/hooks/queries/useMutateDeletePost.ts
@@ -1,26 +1,28 @@
-import {deletePost} from '@/api';
+import {useMutation} from '@tanstack/react-query';
+
+import {deletePost} from '@/api/post';
+import {Marker, UseMutationCustomOptions} from '@/types';
 import queryClient from '@/api/queryClient';
 import {queryKeys} from '@/constants';
-import {Marker, UseMutationCustomOptions} from '@/types';
-import {useMutation} from '@tanstack/react-query';
 
 function useMutateDeletePost(mutationOptions?: UseMutationCustomOptions) {
   return useMutation({
     mutationFn: deletePost,
-    onSuccess: deleteId => {
+    onSuccess: deletedId => {
       queryClient.invalidateQueries({
         queryKey: [queryKeys.POST, queryKeys.GET_POSTS],
       });
       queryClient.invalidateQueries({
         queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
       });
-      //   queryClient.setQueryData<Marker[]>(
-      //     [queryKeys.MARKER, queryKeys.GET_MARKERS],
-      //     existingMarkers => {
-      //       return existingMarkers?.filter(marker => marker.id !== deleteId);
-      //     },
-      //   );
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.POST, queryKeys.GET_SEARCH_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.POST, queryKeys.GET_CALENDAR_POSTS],
+      });
     },
+
     ...mutationOptions,
   });
 }
