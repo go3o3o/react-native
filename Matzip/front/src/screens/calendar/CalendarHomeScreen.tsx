e528b47a72bcb0724ff7b5082de4aaa7d9bafb41
hoyoni <hoyeon.kim2773@gmail.com>
2024-12-19 12:51:47 +0900

    Matzip 앱 > add move to today header button

--- Matzip/front/src/screens/calendar/CalendarHomeScreen.tsx
+++ Matzip/front/src/screens/calendar/CalendarHomeScreen.tsx
@@ -1,19 +1,41 @@
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+import useGetCalendarPosts from '@/hooks/queries/useGetCalendarPosts';
 import Calendar from '@/components/calendar/Calendar';
-import {colors} from '@/constants';
+import EventList from '@/components/calendar/EventList';
 import {getMonthYearDetails, getNewMonthYear} from '@/utils';
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {colors} from '@/constants';
 
 function CalendarHomeScreen() {
   const currentMonthYear = getMonthYearDetails(new Date());
   const [monthYear, setMonthYear] = useState(currentMonthYear);
   const [selectedDate, setSelectedDate] = useState(0);
+  const {
+    data: posts,
+    isPending,
+    isError,
+  } = useGetCalendarPosts(monthYear.year, monthYear.month);
+
+  const moveToToday = () => {
+    setSelectedDate(new Date().getDate());
+    setMonthYear(getMonthYearDetails(new Date()));
+  };
+
+  useEffect(() => {
+    moveToToday();
+  }, []);
+
+  if (isPending || isError) {
+    return <></>;
+  }
 
   const handlePressDate = (date: number) => {
     setSelectedDate(date);
   };
 
   const handleUpdateMonth = (increment: number) => {
+    setSelectedDate(0);
     setMonthYear(prev => getNewMonthYear(prev, increment));
   };
 
@@ -21,10 +43,13 @@ function CalendarHomeScreen() {
     <SafeAreaView style={styles.container}>
       <Calendar
         monthYear={monthYear}
-        onChangeMonth={handleUpdateMonth}
+        schedules={posts}
         selectedDate={selectedDate}
         onPressDate={handlePressDate}
+        onChangeMonth={handleUpdateMonth}
+        moveToToday={moveToToday}
       />
+      <EventList posts={posts[selectedDate]} />
     </SafeAreaView>
   );
 }
