ba0e7a8b5dc0f2ad014c2d0c7a51c60aad2c5a5d
hoyoni <hoyeon.kim2773@gmail.com>
2025-01-09 10:34:52 +0900

    Matzip 앱 > add check platform

--- Matzip/front/src/navigations/drawer/CustomDrawerContent.tsx
+++ Matzip/front/src/navigations/drawer/CustomDrawerContent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   Image,
+  Platform,
   Pressable,
   SafeAreaView,
   StyleSheet,
@@ -12,16 +13,19 @@ import {
   DrawerContentScrollView,
   DrawerItemList,
 } from '@react-navigation/drawer';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import useAuth from '@/hooks/queries/useAuth';
-import {colors} from '@/constants';
+import {colors, mainNavigations, settingNavigations} from '@/constants';
 
 function CustomDrawerContent(props: DrawerContentComponentProps) {
   const {getProfileQuery} = useAuth();
   const {email, nickname, imageUri, kakaoImageUri} = getProfileQuery.data || {};
 
-  const handleLogout = () => {
-    logoutMutation.mutate(null);
+  const handlePressSetting = () => {
+    props.navigation.navigate(mainNavigations.SETTING, {
+      screen: settingNavigations.SETTING_HOME,
+    });
   };
 
   return (
@@ -39,10 +43,28 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
               />
             )}
             {imageUri === null && !!kakaoImageUri && (
-              <Image source={{uri: kakaoImageUri}} style={styles.userImage} />
+              <Image
+                source={{
+                  uri: `${
+                    Platform.OS === 'ios'
+                      ? 'http://localhost:3030/'
+                      : 'http://10.0.2.2:3030/'
+                  }${kakaoImageUri}`,
+                }}
+                style={styles.userImage}
+              />
             )}
             {imageUri !== null && (
-              <Image source={{uri: imageUri}} style={styles.userImage} />
+              <Image
+                source={{
+                  uri: `${
+                    Platform.OS === 'ios'
+                      ? 'http://localhost:3030/'
+                      : 'http://10.0.2.2:3030/'
+                  }${imageUri}`,
+                }}
+                style={styles.userImage}
+              />
             )}
           </Pressable>
 
@@ -50,14 +72,13 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
         </View>
         <DrawerItemList {...props} />
       </DrawerContentScrollView>
-      <Pressable
-        onPress={handleLogout}
-        style={{
-          alignItems: 'flex-end',
-          padding: 10,
-        }}>
-        <Text>로그아웃</Text>
-      </Pressable>
+
+      <View style={styles.bottomContainer}>
+        <Pressable style={styles.bottomMenu} onPress={handlePressSetting}>
+          <MaterialIcons name={'settings'} color={colors.GRAY_700} size={18} />
+          <Text style={styles.bottomMenuText}>설정</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
@@ -88,6 +109,24 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     borderRadius: 35,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderTopWidth: 1,
+    borderTopColor: colors.GRAY_200,
+  },
+  bottomMenu: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
+  },
+  bottomMenuText: {
+    fontWeight: '600',
+    fontSize: 15,
+    color: colors.GRAY_700,
   },
 });
 
