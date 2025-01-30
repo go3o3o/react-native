bfe5a6bcc6ff94de03553552987c443779de4ce2
hoyoni <hoyeon.kim2773@gmail.com>
2025-01-30 17:21:20 +0900

    Matzip 앱 > apply dark mode

--- Matzip/front/src/screens/setting/EditProfileScreen.tsx
+++ Matzip/front/src/screens/setting/EditProfileScreen.tsx
@@ -1,12 +1,14 @@
 import InputField from '@/components/common/InputField';
 import EditProfileHeaderRight from '@/components/setting/EditProfileHeaderRight';
 import EditProfileImageOption from '@/components/setting/EditProfileImageOption';
-import {colors, errorMessages} from '@/constants';
+import {colors, errorMessages, settingNavigations} from '@/constants';
 import useAuth from '@/hooks/queries/useAuth';
 import useForm from '@/hooks/useForm';
 import useImagePicker from '@/hooks/useImagePicker';
 import useModal from '@/hooks/useModal';
 import {SettingStackParamList} from '@/navigations/stack/SettingStackNavigator';
+import useThemeStore from '@/store/useThemeStore';
+import {ThemeMode} from '@/types';
 import {validateEditProfile} from '@/utils';
 import {StackScreenProps} from '@react-navigation/stack';
 import React, {useEffect} from 'react';
@@ -25,6 +27,8 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 type EditProfileScreenProps = StackScreenProps<SettingStackParamList>;
 
 function EditProfileScreen({navigation}: EditProfileScreenProps) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
   const {getProfileQuery, profileMutation} = useAuth();
   const {nickname, imageUri, kakaoImageUri} = getProfileQuery.data || {};
 
@@ -81,7 +85,11 @@ function EditProfileScreen({navigation}: EditProfileScreenProps) {
           style={[styles.imageContainer, styles.emptyImageContainer]}
           onPress={handlePressImage}>
           {imagePicker.imageUris.length === 0 && !kakaoImageUri && (
-            <Ionicons name="camera-outline" size={30} color={colors.GRAY_500} />
+            <Ionicons
+              name="camera-outline"
+              size={30}
+              color={colors[theme].GRAY_500}
+            />
           )}
           {imagePicker.imageUris.length === 0 && kakaoImageUri && (
             <>
@@ -122,8 +130,14 @@ function EditProfileScreen({navigation}: EditProfileScreenProps) {
         touched={editProfile.touched.nickname}
         placeholder="닉네임을 입력해주세요."
       />
-      <Pressable style={styles.deleteAccountContainer}>
-        <Ionicons name="remove-circle-sharp" size={18} color={colors.RED_500} />
+      <Pressable
+        style={styles.deleteAccountContainer}
+        onPress={() => navigation.navigate(settingNavigations.DELETE_ACCOUNT)}>
+        <Ionicons
+          name="remove-circle-sharp"
+          size={18}
+          color={colors[theme].RED_500}
+        />
         <Text style={styles.deleteAccountText}>회원탈퇴</Text>
       </Pressable>
       <EditProfileImageOption
@@ -135,49 +149,50 @@ function EditProfileScreen({navigation}: EditProfileScreenProps) {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  profileContainer: {
-    alignItems: 'center',
-    marginTop: 20,
-    marginBottom: 40,
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 50,
-  },
-  imageContainer: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-  },
-  emptyImageContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: colors.GRAY_200,
-    borderRadius: 50,
-    borderWidth: 1,
-  },
-  deleteAccountContainer: {
-    position: 'absolute',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 5,
-    right: 20,
-    bottom: 70,
-    backgroundColor: colors.GRAY_100,
-    borderRadius: 10,
-    padding: 10,
-  },
-  deleteAccountText: {
-    color: colors.RED_500,
-    fontSize: 15,
-  },
-});
+const styling = (theme: ThemeMode) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+    },
+    profileContainer: {
+      alignItems: 'center',
+      marginTop: 20,
+      marginBottom: 40,
+    },
+    image: {
+      width: '100%',
+      height: '100%',
+      borderRadius: 50,
+    },
+    imageContainer: {
+      width: 100,
+      height: 100,
+      borderRadius: 50,
+    },
+    emptyImageContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderColor: colors[theme].GRAY_200,
+      borderRadius: 50,
+      borderWidth: 1,
+    },
+    deleteAccountContainer: {
+      position: 'absolute',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      gap: 5,
+      right: 20,
+      bottom: 70,
+      backgroundColor: colors[theme].GRAY_100,
+      borderRadius: 10,
+      padding: 10,
+    },
+    deleteAccountText: {
+      color: colors[theme].RED_500,
+      fontSize: 15,
+    },
+  });
 
 export default EditProfileScreen;
