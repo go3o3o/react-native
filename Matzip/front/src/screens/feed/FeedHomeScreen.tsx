50435772304077532d753a1ea5eb88980ab4d5ac
hoyoni <hoyeon.kim2773@gmail.com>
2025-02-05 07:25:42 +0900

    Matzip 앱 > apply suspense

--- Matzip/front/src/screens/feed/FeedHomeScreen.tsx
+++ Matzip/front/src/screens/feed/FeedHomeScreen.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {Suspense} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import FeedList from '@/components/feed/FeedList';
+import Indicator from '@/components/common/Indicator';
 
 export default function FeedHomeScreen() {
   return (
-    <View>
-      <Text>피드</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Suspense fallback={<Indicator />}>
+        <FeedList />
+      </Suspense>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
