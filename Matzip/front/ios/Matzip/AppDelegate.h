138ac1930f5e066af0aae30e133642407cafa06f
hoyoni <hoyeon.kim2773@gmail.com>
2025-02-10 19:01:52 +0900

    Matzip 앱 > add splash icon (ios)

--- Matzip/front/ios/Matzip/AppDelegate.h
+++ Matzip/front/ios/Matzip/AppDelegate.h
@@ -1,6 +1,29 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
+#import "RNSplashScreen.h" 
 
-@interface AppDelegate : RCTAppDelegate
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  [GMSServices provideAPIKey:@"AIzaSyD30G2DIRx_c73iPAegAi7OWSiV1jdZzkw"];
+  self.moduleName = @"Matzip";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
+
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [RNSplashScreen show];
+  return YES;
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
 
 @end
