448c690f4b3a9469e13087ed4323317634ccddd6
hoyoni <hoyeon.kim2773@gmail.com>
2025-02-20 12:13:43 +0900

    [Add] dismiss keyboard

--- Airbnb/screens/Auth/SignIn.js
+++ Airbnb/screens/Auth/SignIn.js
@@ -1,33 +1,46 @@
 import React from "react";
 import styled from "styled-components/native";
+import { StatusBar, KeyboardAvoidingView } from "react-native";
 import Button from "../../components/Auth/Button";
+import DismissKeyboard from "../../components/DismissKeyboard";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
+  align-items: center;
 `;
 
-const TextInput = styled.TextInput``;
+const InputContainer = styled.TextInput`
+  margin-bottom: 30px;
+`;
 
 export default () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const handleSubmit = () => alert(`${username}${password}`);
+
   return (
-    <Container>
-      <Input
-        value={username}
-        placeholder="Username"
-        autoCapitalize="none"
-        stateFn={setUsername}
-      />
-      <Input
-        value={password}
-        placeholder="Password"
-        isPassword={true}
-        stateFn={setPassword}
-      />
-      <Button text={"Sign In"} accent onPress={handleSubmit}></Button>
-    </Container>
+    <DismissKeyboard>
+      <Container>
+        <StatusBar barStyle="dark-content" />
+        <KeyboardAvoidingView behavior="position">
+          <InputContainer>
+            <Input
+              value={username}
+              placeholder="Username"
+              autoCapitalize="none"
+              stateFn={setUsername}
+            />
+            <Input
+              value={password}
+              placeholder="Password"
+              isPassword={true}
+              stateFn={setPassword}
+            />
+          </InputContainer>
+          <Button text={"Sign In"} accent onPress={handleSubmit}></Button>
+        </KeyboardAvoidingView>
+      </Container>
+    </DismissKeyboard>
   );
 };
