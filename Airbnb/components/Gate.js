df8b51517ab118db17d869e8897904c59baad832
hoyoni <hoyeon.kim2773@gmail.com>
2025-02-23 11:53:15 +0900

    [Add] main navigation

--- Airbnb/components/Gate.js
+++ Airbnb/components/Gate.js
@@ -1,22 +1,15 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
-import { logIn, logOut } from "../redux/usersSlice";
 import Auth from "../navigation/Auth";
 import { NavigationContainer } from "@react-navigation/native";
+import Main from "../navigation/Main";
 
 export default () => {
   const { isLoggedIn } = useSelector((state) => state.usersReducer);
   const dispatch = useDispatch();
   return (
     <NavigationContainer>
-      {isLoggedIn ? (
-        <TouchableOpacity onPress={() => dispatch(logOut())}>
-          <Text>Log Out</Text>
-        </TouchableOpacity>
-      ) : (
-        <Auth />
-      )}
+      {isLoggedIn ? <Main /> : <Auth />}
     </NavigationContainer>
   );
 };
