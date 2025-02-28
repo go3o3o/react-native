9b1f86a24cbdc65cabd768a18f114c80b3e5ce47
hoyoni <hoyeon.kim2773@gmail.com>
2025-02-28 14:31:51 +0900

    [Add] photo slider

--- Airbnb/components/RoomCard.js
+++ Airbnb/components/RoomCard.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Pt from "prop-types";
 import styled from "styled-components/native";
+import { Dimensions } from "react-native";
+import Swiper from "react-native-swiper";
+
+const { width, height } = Dimensions.get("screen");
 
 const Container = styled.View`
   width: 100%;
@@ -40,8 +44,40 @@ const PriceNumber = styled.Text`
   font-size: 16px;
 `;
 
+const PhotosContainer = styled.View`
+  margin-bottom: 10px;
+  overflow: hidden;
+  width: 100%;
+  height: ${height / 4}px;
+  border-radius: 4px;
+`;
+
+const SlideImage = styled.Image`
+  width: 100%;
+  height: 100%;
+`;
+
 const RoomCard = ({ id, isFav, isSuperHost, photos, name, price }) => (
   <Container>
+    <PhotosContainer>
+      {photos.length === 0 ? (
+        <SlideImage
+          resizeMode="repeat"
+          source={require("../assets/roomDefault.jpeg")}
+        />
+      ) : (
+        <Swiper
+          removeClippedSubviews
+          paginationStyle={{ marginBottom: -15 }}
+          dotColor={"rgba(200, 200, 200, 0.8)"}
+          activeDotColor={"white"}
+        >
+          {photos.map((photo) => (
+            <SlideImage key={photo.id} source={{ uri: photo.file }} />
+          ))}
+        </Swiper>
+      )}
+    </PhotosContainer>
     {isSuperHost ? (
       <Superhost>
         <SuperhostText>Superhost</SuperhostText>
--- Airbnb/screens/Main/Explore/ExplorePresenter.js
+++ Airbnb/screens/Main/Explore/ExplorePresenter.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { ActivityIndicator, ScrollView } from "react-native";
+import {
+  ActivityIndicator,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import styled from "styled-components/native";
 import RoomCard from "../../../components/RoomCard";
 
@@ -7,9 +12,24 @@ const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  padding-horizontal: 15px;
 `;
 
-const Text = styled.Text``;
+const FakeBar = styled.View`
+  height: 40px;
+  width: 100%;
+  background-color: white;
+  box-shadow: 1px 5px 5px rgba(200, 200, 200, 0.5);
+  margin: 80px 0px 10px 0px;
+  border-radius: 7px;
+  justify-content: center;
+  padding-left: 10px;
+`;
+
+const FakeText = styled.Text`
+  font-size: 14px;
+  font-weight: 300;
+`;
 
 export default ({ rooms }) => {
   return (
@@ -17,22 +37,31 @@ export default ({ rooms }) => {
       {rooms.length === 0 ? (
         <ActivityIndicator color="black" />
       ) : (
-        <ScrollView
-          style={{ width: "100%", marginTop: 120 }}
-          contentContainerStyle={{ paddingHorizontal: 15 }}
-        >
-          {rooms.map((room) => (
-            <RoomCard
-              key={room.id}
-              name={room.name}
-              price={room.price}
-              photos={room.photos}
-              id={room.id}
-              isFav={room.is_fav}
-              isSuperHost={room.user.superhost}
-            />
-          ))}
-        </ScrollView>
+        <>
+          <FakeBar>
+            <FakeText>Search...</FakeText>
+          </FakeBar>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            style={{ width: "100%" }}
+            contentContainerStyle={{ paddingTop: 30 }}
+          >
+            {rooms.map((room) => (
+              <RoomCard
+                key={room.id}
+                name={room.name}
+                price={room.price}
+                photos={room.photos}
+                id={room.id}
+                isFav={room.is_fav}
+                isSuperHost={room.user.superhost}
+              />
+            ))}
+            <TouchableOpacity>
+              <Text>Load More</Text>
+            </TouchableOpacity>
+          </ScrollView>
+        </>
       )}
     </Container>
   );
