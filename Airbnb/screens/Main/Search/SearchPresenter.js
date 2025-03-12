2c773d9fcfb931890f04056e3baee749a036d19c
hoyoni <hoyeon.kim2773@gmail.com>
2025-03-12 12:48:08 +0900

    [fix] add filter

--- Airbnb/screens/Main/Search/SearchPresenter.js
+++ Airbnb/screens/Main/Search/SearchPresenter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import styled from "styled-components/native";
 import DismissKeyboard from "../../../components/DismissKeyboard";
@@ -29,6 +30,31 @@ const CancelContainer = styled.TouchableOpacity``;
 
 const CancelText = styled.Text``;
 
+const FiltersContainer = styled.ScrollView`
+  flex-direction: row;
+  margin-top: 10px;
+`;
+
+const FilterContainer = styled.View`
+  align-items: center;
+  margin-right: 15px;
+`;
+
+const FilterLabel = styled.Text`
+  text-transform: uppercase;
+  font-size: 12px;
+  margin-bottom: 5px;
+  font-weight: 500;
+`;
+
+const Filter = styled.TextInput`
+  padding: 10px;
+  background-color: white;
+  border-radius: 20px;
+  box-shadow: 1px 2.5px 2.5px rgba(200, 200, 200, 0.5);
+  width: 80px;
+`;
+
 export default () => {
   const navigation = useNavigation();
   return (
@@ -40,6 +66,28 @@ export default () => {
             <CancelText>Cancel</CancelText>
           </CancelContainer>
         </SearchContainer>
+        <FiltersContainer
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingVertical: 10, paddingHorizontal: 20 }}
+        >
+          <FilterContainer>
+            <FilterLabel>Beds</FilterLabel>
+            <Filter placeholder="0" keyboardType={"number-pad"} />
+          </FilterContainer>
+          <FilterContainer>
+            <FilterLabel>Bedrooms</FilterLabel>
+            <Filter placeholder="0" keyboardType={"number-pad"} />
+          </FilterContainer>
+          <FilterContainer>
+            <FilterLabel>Bathrooms</FilterLabel>
+            <Filter placeholder="0" keyboardType={"number-pad"} />
+          </FilterContainer>
+          <FilterContainer>
+            <FilterLabel>Max. price</FilterLabel>
+            <Filter placeholder="$0" keyboardType={"number-pad"} />
+          </FilterContainer>
+        </FiltersContainer>
       </Container>
     </DismissKeyboard>
   );
