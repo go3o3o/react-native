a76561bb9852e3665ac013f41a2be797ed24335b
hoyoni <hoyeon.kim2773@gmail.com>
2025-03-18 18:29:02 +0900

    [Fix] animating the camera

--- Airbnb/screens/Main/Map.js
+++ Airbnb/screens/Main/Map.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components/native";
 import { StyleSheet, Dimensions } from "react-native";
 import { connect } from "react-redux";
@@ -55,7 +55,9 @@ const RoomPrice = styled.Text`
 `;
 
 const Map = ({ rooms }) => {
+  const mapRef = useRef();
   const [currentIndex, setCurrentIndex] = useState(0);
+
   const onScroll = (e) => {
     const {
       nativeEvent: {
@@ -65,16 +67,32 @@ const Map = ({ rooms }) => {
     const position = Math.abs(Math.round(x / width));
     setCurrentIndex(position);
   };
+
+  useEffect(() => {
+    if (currentIndex !== 0) {
+      mapRef.current?.animateCamera(
+        {
+          center: {
+            latitude: parseFloat(rooms[currentIndex].lat),
+            longitude: parseFloat(rooms[currentIndex].lng),
+          },
+        },
+        { duration: 3000 }
+      );
+    }
+  }, [currentIndex]);
+
   return (
     <Container>
       <MapView
+        ref={mapRef}
         style={StyleSheet.absoluteFill}
         camera={{
           center: {
             latitude: parseFloat(rooms[0].lat),
             longitude: parseFloat(rooms[0].lng),
           },
-          altitude: 1000,
+          altitude: 2000,
           pitch: 0,
           heading: 0,
           zoom: 10,
