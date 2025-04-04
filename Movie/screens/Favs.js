61bbe7e99d07f7295ff58841f40e7fa6f1c84397
hoyoni <hoyeon.kim2773@gmail.com>
2025-04-04 15:27:30 +0900

    [Add] TV and discovery data

--- Movie/screens/Favs.js
+++ Movie/screens/Favs.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 
-export default () => (
-  <View>
-    <Text>Favs</Text>
-  </View>
-);
+export default () => {
+  const [movies, setMovies] = useState({
+    results: [],
+    error: null,
+  });
+  const getData = async () => {
+    const [results, error] = await movieApi.discover();
+    setMovies({
+      results,
+      error,
+    });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <View>
+      <Text>{movies.results.length}</Text>
+    </View>
+  );
+};
