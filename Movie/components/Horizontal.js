b7258271da85a6ebe6797afe04c5dea9dceda45e
hoyoni <hoyeon.kim2773@gmail.com>
2025-05-02 18:21:35 +0900

    [Fix] going to detail

--- Movie/components/Horizontal.js
+++ Movie/components/Horizontal.js
@@ -4,6 +4,7 @@ import styled from "styled-components/native";
 import PropTypes from "prop-types";
 import Poster from "./Poster";
 import { trimText, formatDate } from "../utils";
+import { useNavigation } from "@react-navigation/native";
 
 const Container = styled.View`
   padding: 0px 30px;
@@ -36,20 +37,32 @@ const Overview = styled.Text`
   opacity: 0.8;
 `;
 
-const Horizontal = ({ id, title, poster, overview, releaseDate }) => (
-  <TouchableOpacity>
-    <Container>
-      <Poster url={poster} />
-      <Data>
-        <Title>{trimText(title, 30)}</Title>
-        {releaseDate ? (
-          <ReleaseDate>{formatDate(releaseDate)}</ReleaseDate>
-        ) : null}
-        <Overview>{trimText(overview, 80)}</Overview>
-      </Data>
-    </Container>
-  </TouchableOpacity>
-);
+const Horizontal = ({ id, title, poster, overview, releaseDate }) => {
+  const navigation = useNavigation();
+  const goToDetail = () => {
+    navigation.navigate("Detail", {
+      id,
+      title,
+      poster,
+      overview,
+      releaseDate,
+    });
+  };
+  return (
+    <TouchableOpacity onPress={goToDetail}>
+      <Container>
+        <Poster url={poster} />
+        <Data>
+          <Title>{trimText(title, 30)}</Title>
+          {releaseDate ? (
+            <ReleaseDate>{formatDate(releaseDate)}</ReleaseDate>
+          ) : null}
+          <Overview>{trimText(overview, 80)}</Overview>
+        </Data>
+      </Container>
+    </TouchableOpacity>
+  );
+};
 
 Horizontal.propTypes = {
   id: PropTypes.number.isRequired,
